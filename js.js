21e5755ea8bad2f66e4082a4930ece605f6d647d
goffart jean marc <kithulovalibin@gmail.com>
2024-10-18 20:58:26 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,29 @@
+
+// let stoke=document.getElementById("stoke");
+// let count=0
+// function jean(){
+//     count=count+1
+//     stoke.innerText=count
+// }
+//  function save(){
+//     console.log(count)
+//  }
+//  let username="you have received 3 notification"
+//  console.log(username)
+// let name="goffart"
+// let greeting="hello how are you going today"
+// let mygreeting=greeting+"," +name
+// console.log(mygreeting)
+// let name="jean marc"
+// let meet="hi"
+// let final_line= meet+","+name
+// console.log(final_line)
+  
+// let welcomeEl=document.getElementById(" welcome-el")
+// let name ="jean marc"
+// let greeting="hello welcome at home"
+// welcomeEl.innerText= greeting+name
+let welcome=document.getElementById(" welcome")
+let name="jean marc"
+let greeting="you are welcome t"
+welcome.innerHTML= greeting+name
