1d24dbe39458008187278af28de824a4e5e0e2d6
goffart jean marc <kithulovalibin@gmail.com>
2024-10-18 11:39:28 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+// let count :5
+// console.log(count) let my age= 45
+let humandogratio=67
+let mydogage=myage*humandogratio
+console.log(mydogage)
+jean=jean+50
+jean=jean-20
+jean=jean+70
+console.log(jean)
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+function numbers(){
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+numbers()
+numbers()
+function home() {
+    // console.log(42)
+    let lap1=34
+    let lap2=33
+    let lap3=36
+    console.log(lap1+lap2+lap3)
+}
+home()
+let just=0
+let lap1=just+1
+let lap2=lap1+1
+let lap3=lap2+1
+function add(){
+   console.log(lap1)
+   console.log(lap2)
+   console.log(lap3)
+}
+add()
+let countEl=document.getElementById("count-el")
+console.log(countEl)
+let count= 0
+function increment()
+{
+    count=count+1
+    countEl.innerText= count
+}
