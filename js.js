01f118d17265535571577c290660607227cb0a11
goffart jean marc <kithulovalibin@gmail.com>
2024-10-19 14:44:35 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,18 @@
 
-// let stoke=document.getElementById("stoke");
-// let count=0
-// function jean(){
-//     count=count+1
-//     stoke.innerText=count
-// }
-//  function save(){
-//     console.log(count)
-//  }
+ let stoke=document.getElementById("stoke");
+ let saveEl=document.getelementByid("save-el")
+ let count=0
+ function jean(){
+     count=count+1
+     stoke.innerText=count
+ }
+ function save(){
+    let countStr=count +"-"
+    saveEl.innerText=countStr
+    console.log(count)
+  }
+
+
 //  let username="you have received 3 notification"
 //  console.log(username)
 // let name="goffart"
@@ -17,13 +22,13 @@
 // let name="jean marc"
 // let meet="hi"
 // let final_line= meet+","+name
-// console.log(final_line)
-  
-// let welcomeEl=document.getElementById(" welcome-el")
-// let name ="jean marc"
-// let greeting="hello welcome at home"
-// welcomeEl.innerText= greeting+name
-let welcome=document.getElementById(" welcome")
+//  console.log(final_line)
+  /* 
+ let welcomeEl=document.getElementById(" welcome-el")
+ let name ="jean marc"
+let greeting="hello welcome at home"
+welcomeEl.innerText= greeting+name */
+/* let welcome=document.getElementById(" welcome")
 let name="jean marc"
 let greeting="you are welcome t"
-welcome.innerHTML= greeting+name
+welcome.innerHTML= greeting+name */
